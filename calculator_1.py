4472b198d6b154c02addc87d5adc7772c8e0ece2
nemrof <nemrofe@gmail.com>
2021-02-13 17:56:03 +0100

    Changes to UI

--- calculator_1.py
+++ calculator_1.py
@@ -66,42 +66,19 @@ def multiply(*argv):
 # ------- UI -------
 # layout initial settings
 # dictionary keys for buttons layout
-bn = {'size': (7, 2), 'font': ('Computer Modern', 24), 'button_color': ("black", "#ccf2ff"), 'border_width': '4'}
+bn = {'size': (7, 2), 'font': ('Computer Modern', 24), 'button_color': ("black", "#ccf2ff"), 'border_width': '0'}
+bf = {'size': (7, 2), 'font': ('Computer Modern', 24), 'button_color': ("black", "#d6f5f5"), 'border_width': '0'}
+bs = {'size': (15, 2), 'font': ('Computer Modern', 24), 'button_color': ("black", "#d6f5f5"), 'border_width': '0'}
 
 
-file_list_column = [
-    [sg.Text('PyCalculator',size=(50,1), justification='right', background_color='#ccffff', text_color='black')],
-    [sg.Button(button_text='7', **bn), sg.Button(button_text='8', **bn), sg.Button(button_text='9', **bn)],
-    [
-        sg.Button(button_text='4', **bn),
-        sg.Button(button_text='5', **bn),
-        sg.Button(button_text='6', **bn)
-    ],
-    [
-        sg.Button(button_text='1', **bn),
-        sg.Button(button_text='2', **bn),
-        sg.Button(button_text='3', **bn)
-    ],
-]
-# For now will only show the name of the file that was chosen
-numberFunctions = [
-    [sg.Button(button_text="+")],
-    [sg.Button(button_text="-")],
-    [sg.Button(button_text="/")],
-    [sg.Button(button_text="*")],
-    #[sg.Text(size=(40, 1), key="-TOUT-")],
-    #[sg.Image(key="-IMAGE-")],
-]
-# ----- Full layout -----
 layout = [
-    [
-        sg.Column(file_list_column),
-        sg.Column(numberFunctions),
-    ]
+[sg.Text('PyCalculator', size=(50, 1), justification='right', background_color='#ccffff', text_color='black')],
+    [sg.Button(button_text="+", **bf), sg.Button(button_text="-", **bf), sg.Button(button_text="/", **bf), sg.Button(button_text="*", **bf)],
+    [sg.Button(button_text='7', **bn), sg.Button(button_text='8', **bn), sg.Button(button_text='9', **bn), sg.Button(button_text='C', **bn)],
+    [sg.Button(button_text='4', **bn), sg.Button(button_text='5', **bn), sg.Button(button_text='6', **bn), sg.Button(button_text='CE', **bn)],
+    [sg.Button(button_text='1', **bn), sg.Button(button_text='2', **bn), sg.Button(button_text='3', **bn), sg.Button(button_text='%', **bn)],
+    [sg.Button(button_text='0', **bn), sg.Button(button_text='.', **bn), sg.Button(button_text='=', **bs)],
 ]
-
-
-#layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
 
 # Create the window
 window = sg.Window("Calculator", layout)
